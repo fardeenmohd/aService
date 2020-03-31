6cfcadaea1db1a8f14b70c6c09b91dfe5fc3056f
fardeenmohd <fardeenmohdpl@gmail.com>
2020-03-31 17:38:06 +0200

    Add alpha vantage API support Change static data to sample data from the api

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,17 +1,22 @@
 import math
 import numpy as np
 import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
 
-class DataLoader():
+
+class DataLoader:
     """A class for loading and transforming data for the lstm model"""
 
     def __init__(self, filename, split, cols):
-        dataframe = pd.read_csv(filename)
+        ts = TimeSeries(key='T6P5PZDEXZTMHC5V', output_format='pandas')
+        dataframe, meta_data = ts.get_intraday(symbol='MSFT', interval='1min', outputsize='full')
+
+        # dataframe = pd.read_csv(filename)
         i_split = int(len(dataframe) * split)
         self.data_train = dataframe.get(cols).values[:i_split]
-        self.data_test  = dataframe.get(cols).values[i_split:]
-        self.len_train  = len(self.data_train)
-        self.len_test   = len(self.data_test)
+        self.data_test = dataframe.get(cols).values[i_split:]
+        self.len_train = len(self.data_train)
+        self.len_test = len(self.data_test)
         self.len_train_windows = None
 
     def get_test_data(self, seq_len, normalise):
@@ -22,14 +27,14 @@ class DataLoader():
         '''
         data_windows = []
         for i in range(self.len_test - seq_len):
-            data_windows.append(self.data_test[i:i+seq_len])
+            data_windows.append(self.data_test[i:i + seq_len])
 
         data_windows = np.array(data_windows).astype(float)
         data_windows = self.normalise_windows(data_windows, single_window=False) if normalise else data_windows
 
         x = data_windows[:, :-1]
         y = data_windows[:, -1, [0]]
-        return x,y
+        return x, y
 
     def get_train_data(self, seq_len, normalise):
         '''
@@ -64,7 +69,7 @@ class DataLoader():
 
     def _next_window(self, i, seq_len, normalise):
         '''Generates the next data window from the given index location i'''
-        window = self.data_train[i:i+seq_len]
+        window = self.data_train[i:i + seq_len]
         window = self.normalise_windows(window, single_window=True)[0] if normalise else window
         x = window[:-1]
         y = window[-1, [0]]
@@ -79,6 +84,7 @@ class DataLoader():
             for col_i in range(window.shape[1]):
                 normalised_col = [((float(p) / float(window[0, col_i])) - 1) for p in window[:, col_i]]
                 normalised_window.append(normalised_col)
-            normalised_window = np.array(normalised_window).T # reshape and transpose array back into original multidimensional format
+            normalised_window = np.array(
+                normalised_window).T  # reshape and transpose array back into original multidimensional format
             normalised_data.append(normalised_window)
         return np.array(normalised_data)
